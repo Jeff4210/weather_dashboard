17073155c659cd750d32fc4bbfc26a51c9e529d4
jeff <jeffreyroelofse@icloud.com>
2025-07-16 23:23:52 -0700

    Removed notes file

--- static/note.swift
+++
@@ -1,6 +0,0 @@
-document.querySelector('nav.navbar ul.menu li.has-children').classList.add('open');
-
-document.querySelector('nav.navbar ul.menu li.has-children').style.outline = '2px solid lime';
-
-document.querySelector('nav.navbar ul.menu li.has-children > ul.submenu').style.display = 'block';
-
